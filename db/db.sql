033bdbf03f5110eac19c946a12c38f40a004203a
javiddelossantos5 <javiddelossantos5@gmail.com>
2021-04-03 22:32:45 -0600

    added folder db with functionality for making a database in SQL

---
+++ db/db.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE election;
+DROP DATABASE IF EXISTS election;
+
+USE election;
---
+++ utils/inputCheck.js
@@ -0,0 +1,31 @@
+// utility to check if an object has the required properties
+// e.g., inputCheck(object, 'prop1', 'prop2', 'etc')
+
+module.exports = function(obj, ...props) {
+  const errors = [];
+
+  props.forEach((prop) => {
+    // if property is blank or doesn't exist, add to errors array
+    if (obj[prop] === undefined || obj[prop] === '') {
+      errors.push(`No ${prop} specified.`);
+    }
+  });
+
+  if (errors.length) {
+    return {
+      error: errors.join(' ')
+    };
+  }
+  
+  return null;
+};
+
+// export PATH="${PATH}:/usr/local/mysql/bin/"
+// mysql -u root -p
+// CREATE TABLE candidates (
+//   ->   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+//   ->   first_name VARCHAR(30) NOT NULL,
+//   ->   last_name VARCHAR(30) NOT NULL,
+//   ->   industry_connected BOOLEAN NOT NULL
+//   -> );
+// SELECT * FROM candidates;
